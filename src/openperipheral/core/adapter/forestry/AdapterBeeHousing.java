d9140621e7cf217c7810c85c4dbb6e4cb2afec13
Mikee Franklin <mikeefranklin@gmail.com>
2013-08-30 12:34:58 +0100

    added getQueen

--- src/openperipheral/core/adapter/forestry/AdapterBeeHousing.java
+++ src/openperipheral/core/adapter/forestry/AdapterBeeHousing.java
@@ -19,18 +19,23 @@ public class AdapterBeeHousing implements IPeripheralAdapter {
 	public Class getTargetClass() {
 		return IBeeHousing.class;
 	}
-	
+
 	@LuaMethod(returnType = LuaType.BOOLEAN, description = "Can the bees breed?")
 	public boolean canBreed(IComputerAccess computer, IBeeHousing beeHousing) {
 		return beeHousing.canBreed();
 	}
-	
+
 	@LuaMethod(returnType = LuaType.TABLE, description = "Get the drone")
 	public IIndividual getDrone(IComputerAccess computer, IBeeHousing beeHousing) {
 		ItemStack drone = beeHousing.getDrone();
-		if (drone != null) {
-			return AlleleManager.alleleRegistry.getIndividual(drone);
-		}
+		if (drone != null) { return AlleleManager.alleleRegistry.getIndividual(drone); }
+		return null;
+	}
+
+	@LuaMethod(returnType = LuaType.TABLE, description = "Get the queen")
+	public IIndividual getQueen(IComputerAccess computer, IBeeHousing beeHousing) {
+		ItemStack queen = beeHousing.getQueen();
+		if (queen != null) { return AlleleManager.alleleRegistry.getIndividual(queen); }
 		return null;
 	}
 
