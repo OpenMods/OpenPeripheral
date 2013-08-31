0fa5496f39a188a351055ac06e73a27441a69c0f
Mikee Franklin <mikeefranklin@gmail.com>
2013-08-31 14:27:46 +0100

    added getJobList. not sure yet if this'll stay

---
+++ src/openperipheral/core/adapter/appeng/AdapterTileController.java
@@ -0,0 +1,31 @@
+package openperipheral.core.adapter.appeng;
+
+import java.util.List;
+
+import dan200.computer.api.IComputerAccess;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+
+import openperipheral.api.IPeripheralAdapter;
+import openperipheral.api.LuaMethod;
+import openperipheral.api.LuaType;
+import openperipheral.core.util.CallWrapper;
+
+public class AdapterTileController implements IPeripheralAdapter {
+
+	@Override
+	public Class getTargetClass() {
+		try {
+			return Class.forName("appeng.me.tile.TileController");
+		}catch (Exception e) {
+		}
+		return null;
+	}
+	
+	@LuaMethod(description="Get the current job list", returnType=LuaType.TABLE)
+	public List<ItemStack> getJobList(IComputerAccess computer, TileEntity controller) {
+		return new CallWrapper<List<ItemStack>>().call(controller, "getJobList");
+	}
+
+}
--- src/openperipheral/core/integration/ModuleAppEng.java
+++ src/openperipheral/core/integration/ModuleAppEng.java
@@ -4,6 +4,7 @@ import openperipheral.core.AdapterManager;
 import openperipheral.core.TypeConversionRegistry;
 import openperipheral.core.adapter.appeng.AdapterCellProvider;
 import openperipheral.core.adapter.appeng.AdapterGridInterface;
+import openperipheral.core.adapter.appeng.AdapterTileController;
 import openperipheral.core.converter.ConverterIItemList;
 
 public class ModuleAppEng {
@@ -11,6 +12,7 @@ public class ModuleAppEng {
 	public static void init() {
 		AdapterManager.addPeripheralAdapter(new AdapterCellProvider());
 		AdapterManager.addPeripheralAdapter(new AdapterGridInterface());
+		AdapterManager.addPeripheralAdapter(new AdapterTileController());
 		TypeConversionRegistry.registerTypeConverter(new ConverterIItemList());
 	}
 
