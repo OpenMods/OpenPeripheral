d16b4d78abe2b90f13e5cbc74cf911f01be5bb4f
Foone Turing <foone@soomopublishing.com>
2013-09-18 11:15:57 -0400

    Added support for MystCraft pages and improved Linking Book support and fixed it for the current MystCraft

--- src/openperipheral/core/integration/ModuleMystcraft.java
+++ src/openperipheral/core/integration/ModuleMystcraft.java
@@ -1,13 +1,17 @@
 package openperipheral.core.integration;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
 import openperipheral.core.AdapterManager;
 import openperipheral.core.adapter.mystcraft.AdapterWritingDesk;
 
-import com.xcompwiz.mystcraft.api.items.IItemRenameable;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
+import cpw.mods.fml.common.FMLLog;
 
 public class ModuleMystcraft {
 
@@ -15,15 +19,42 @@ public class ModuleMystcraft {
     AdapterManager.addPeripheralAdapter(new AdapterWritingDesk());
 
   }
-	public static void appendMystcraftInfo(Map map, ItemStack stack) {
-		if (stack != null && stack.getItem() instanceof IItemRenameable) {
-			if (stack.hasTagCompound()) {
-				NBTTagCompound tag = stack.getTagCompound();
-				if (tag.hasKey("agename")) {
-					map.put("destination", tag.getString("agename"));
-				}
-			}
-		}
-		
+
+  public static void appendMystcraftInfo(Map map, ItemStack stack) {
+	  if (stack != null){
+	    Item item = stack.getItem();
+	    if (item != null){
+	      
+	      if ("item.myst.page".equals(item.getUnlocalizedName())){
+	        addStringFromNBT(map, stack, "symbol", "symbol");
+	      }else if ("item.myst.linkbook".equals(item.getUnlocalizedName())){
+	        addStringFromNBT(map, stack, "destination", "agename");
+	        addLinkingBookFlags(map, stack);
+	      }
+	    }
+	  }
 	}
+	
+  private static void addLinkingBookFlags(Map map, ItemStack stack) {
+    Map<String,Boolean> flags = new HashMap<String,Boolean>();
+    map.put("flags", flags);
+    if (stack.hasTagCompound()) {
+      NBTTagCompound tag = stack.getTagCompound();
+      if (tag.hasKey("Flags")){
+        for(NBTBase s:(Collection<NBTBase>)tag.getCompoundTag("Flags").getTags()){
+          flags.put(s.getName(), Boolean.TRUE);
+        }
+        
+      }
+    }         
+  }
+  
+  private static void addStringFromNBT(Map map, ItemStack stack, String outputName, String nbtTagName) {
+    if (stack.hasTagCompound()) {
+      NBTTagCompound tag = stack.getTagCompound();
+      if (tag.hasKey(nbtTagName)) {
+        map.put(outputName, tag.getString(nbtTagName));
+      }
+    }
+  }
 }
