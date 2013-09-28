e0124c1606ac02f849e04932c8bfa37dea16d88b
Josh Lloyd <j.nevercast@gmail.com>
2013-09-28 13:24:59 +1200

    Rendering blocks

--- src/openperipheral/glasses/drawable/DrawableIcon.java
+++ src/openperipheral/glasses/drawable/DrawableIcon.java
@@ -9,9 +9,13 @@ import cpw.mods.fml.client.FMLClientHandler;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 
@@ -205,15 +209,60 @@ public class DrawableIcon extends BaseDrawable {
         
         try{
             if(id >= 0 && ( id < Item.itemsList.length && Item.itemsList[id] != null || id < Block.blocksList.length && Block.blocksList[id] != null)) {
-                renderItemIntoGUI(FMLClientHandler.instance().getClient().fontRenderer, clientStack, x, y, (float)alpha, (float)scale);
+                renderIcon();
             }
         }catch(Exception ex) {
             ex.printStackTrace();
         }
     }
     
-    /* Thanks Pahi- for your helpers :) -NC */
-    public static void renderItemIntoGUI(FontRenderer fontRenderer, ItemStack itemStack, int x, int y, float opacity, float scale) {
+    public void renderIcon() {
+        FontRenderer renderer = FMLClientHandler.instance().getClient().fontRenderer;
+        ItemStack stack = clientStack;
+        if(stack != null && stack.getItem() != null) {
+            if(stack.getItem() instanceof ItemBlock) {
+                renderRotatingBlockIntoGUI(renderer, clientStack, this.x, this.y, this.zIndex, (float)this.alpha, (float)this.scale, 30);
+            }else{
+                renderItemIntoGUI(renderer, clientStack, this.x, this.y, this.zIndex, (float)this.alpha, (float)this.scale);
+            }
+        }
+    }
+    
+    /* Thanks Pahi- for your helpers :) P.S. I love your Dev Environment, and congratulations also. -NC */
+    private static void renderRotatingBlockIntoGUI(FontRenderer fontRenderer, ItemStack stack, int x, int y, float zLevel, float alpha, float scale, float angle) {
+
+        RenderBlocks renderBlocks = new RenderBlocks();
+
+        Block block = Block.blocksList[stack.itemID];
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
+        GL11.glPushMatrix();
+        //GL11.glEnable(GL11.GL_LIGHTING);
+        RenderHelper.enableGUIStandardItemLighting();
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
+        GL11.glTranslatef(x - 2, y + 3, -3.0F + zLevel);
+        GL11.glScalef(10.0F, 10.0F, 10.0F);
+        GL11.glTranslatef(1.0F, 0.5F, 1.0F);
+        GL11.glScalef(1.0F * scale, 1.0F * scale, -1.0F);
+        GL11.glRotatef(210.0F, 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(0F + 1 * angle, 0.0F, 1.0F, 0.0F);
+        angle = (angle + 1) % 360;
+
+        int var10 = Item.itemsList[stack.itemID].getColorFromItemStack(stack, 0);
+        float var16 = (var10 >> 16 & 255) / 255.0F;
+        float var12 = (var10 >> 8 & 255) / 255.0F;
+        float var13 = (var10 & 255) / 255.0F;
+
+        GL11.glColor4f(var16, var12, var13, alpha);
+
+        GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F);
+        renderBlocks.useInventoryTint = false; // Inventory Tint prevents the use of Alpha
+        renderBlocks.renderBlockAsItem(block, stack.getItemDamage(), 1.0F);
+        RenderHelper.disableStandardItemLighting();
+        //GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glPopMatrix();
+    }
+
+    private static void renderItemIntoGUI(FontRenderer fontRenderer, ItemStack itemStack, int x, int y, float zLevel, float opacity, float scale) {
         Icon icon = itemStack.getIconIndex();
         GL11.glDisable(GL11.GL_LIGHTING);
         FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
@@ -222,12 +271,12 @@ public class DrawableIcon extends BaseDrawable {
         float green = (overlayColour >> 8 & 255) / 255.0F;
         float blue = (overlayColour & 255) / 255.0F;
         GL11.glColor4f(red, green, blue, opacity);
-        drawTexturedQuad(x, y, icon, 16 * scale, 16 * scale, -90);
+        drawTexturedQuad(x, y, icon, 16 * scale, 16 * scale, zLevel);
         GL11.glEnable(GL11.GL_LIGHTING);
 
     }
 
-    public static void drawTexturedQuad(int x, int y, Icon icon, float width, float height, double zLevel) {
+    private static void drawTexturedQuad(int x, int y, Icon icon, float width, float height, double zLevel) {
 
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
