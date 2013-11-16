66e0e0c6911cef892d8e6e6a5f64a65425021a8d
Trent Monahan <sinz@d3s.co>
2013-11-17 01:26:33 +1100

    Added IEnergyInfo to TE Integration

--- src/openperipheral/core/integration/ModuleThermalExpansion.java
+++ src/openperipheral/core/integration/ModuleThermalExpansion.java
@@ -9,11 +9,13 @@ import net.minecraft.item.ItemStack;
 import openperipheral.core.AdapterManager;
 import openperipheral.core.adapter.thermalexpansion.AdapterEnderAttuned;
 import openperipheral.core.adapter.thermalexpansion.AdapterEnergyHandler;
+import openperipheral.core.adapter.thermalexpansion.AdapterEnergyInfo;
 
 public class ModuleThermalExpansion {
 	public static void init() {
 		AdapterManager.addPeripheralAdapter(new AdapterEnergyHandler());
 		AdapterManager.addPeripheralAdapter(new AdapterEnderAttuned());
+		AdapterManager.addPeripheralAdapter(new AdapterEnergyInfo());
 	}
 	
 	public static void appendRFEnergyInfo(Map map, ItemStack stack) {
