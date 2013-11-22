677e98ff6eb7a69addb7b56fcae76b0a932553ea
Joshua Asbury <theoriginalbit@me.com>
2013-11-23 04:00:24 +1100

    oops missed a file

---
+++ src/openperipheral/ModInfo.java
@@ -0,0 +1,10 @@
+package openperipheral;
+
+public class ModInfo {
+	public static final String ID = "OpenPeripheral";
+	public static final String NAME = "OpenPeripheral";
+	public static final String VERSION = "@VERSION@";
+	public static final String PROXY_SERVER = "openperipheral.core.CommonProxy";
+	public static final String PROXY_CLIENT = "openperipheral.core.client.ClientProxy";
+	public static final String DEPENDENCIES = "required-after:ComputerCraft;after:AppliedEnergistics;after:IC2;after:EnderStorage;after:BuildCraft|Core;after:Forestry;after:Mystcraft;after:ProjRed|Transmission;after:Railcraft;after:Thaumcraft;after:ThermalExpansion;after:SGCraft;";
+}
