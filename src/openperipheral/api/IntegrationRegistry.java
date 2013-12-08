e4683cfd9b0a4292ff3610c30650f40c99fc0a2c
Bartek Bok <bartek.bok@gmail.com>
2013-12-08 21:13:12 +0100

    Backward compatible API - maybe this time it will work

--- src/openperipheral/api/IntegrationRegistry.java
+++ src/openperipheral/api/IntegrationRegistry.java
@@ -1,46 +1,24 @@
 package openperipheral.api;
 
-import java.lang.reflect.Method;
-
+/**
+ * @deprecated Use methods from {@link OpenPeripheralAPI}
+ */
+@Deprecated
 public class IntegrationRegistry {
 
 	/**
-	 * @deprecated Use static version {@link #register(IPeripheralAdapter)}
+	 * @deprecated Use static version {@link OpenPeripheralAPI#register(IPeripheralAdapter)}
 	 */
 	@Deprecated
 	public boolean registerAdapter(IPeripheralAdapter adapter) {
-		return register(adapter);
+		return OpenPeripheralAPI.register(adapter);
 	}
 
 	/**
-	 * @deprecated Use static version {@link #register(ITypeConverter)}
+	 * @deprecated Use static version {@link OpenPeripheralAPI#register(ITypeConverter)}
 	 */
 	@Deprecated
 	public boolean registerTypeConverter(ITypeConverter converter) {
-		return register(converter);
-	}
-
-	public static boolean register(IPeripheralAdapter adapter) {
-		return register(IPeripheralAdapter.class, adapter, "openperipheral.AdapterManager", "addPeripheralAdapter");
-	}
-
-	public static boolean register(ITypeConverter converter) {
-		return register(ITypeConverter.class, converter, "openperipheral.TypeConversionRegistry", "registerTypeConverter");
-	}
-
-	public static boolean register(IIntegrationModule module) {
-		return register(IIntegrationModule.class, module, "openperipheral.IntegrationModuleRegistry", "registerModule");
-	}
-
-	private static boolean register(Class<?> type, Object obj, String klazzName, String methodName) {
-		try {
-			Class<?> klazz = Class.forName(klazzName);
-			if (klazz != null) {
-				Method method = klazz.getMethod(methodName, new Class[] { type });
-				method.invoke(null, obj);
-				return true;
-			}
-		} catch (Exception e) {}
-		return false;
+		return OpenPeripheralAPI.register(converter);
 	}
 }
