af840a06995ea6d0b5de70400dae1ace3c92fef1
Bartek Bok <bartek.bok@gmail.com>
2014-02-25 21:49:06 +0100

    Cleanup AdapterAspectContainer

--- src/openperipheral/integration/thaumcraft/AdapterAspectContainer.java
+++ src/openperipheral/integration/thaumcraft/AdapterAspectContainer.java
@@ -1,16 +1,18 @@
 package openperipheral.integration.thaumcraft;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import openperipheral.api.IPeripheralAdapter;
-import openperipheral.api.LuaMethod;
-import openperipheral.api.LuaType;
+import openperipheral.api.*;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.aspects.IAspectContainer;
-import dan200.computer.api.IComputerAccess;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+@Prefixed("target")
 public class AdapterAspectContainer implements IPeripheralAdapter {
 	@Override
 	public Class<?> getTargetClass() {
@@ -18,18 +20,30 @@ public class AdapterAspectContainer implements IPeripheralAdapter {
 	}
 
 	@LuaMethod(returnType = LuaType.TABLE, description = "Get the Aspects stored in the block")
-	public ArrayList<HashMap<String, Object>> getAspects(IComputerAccess computer, IAspectContainer container) {
-		if (container == null) { return null; }
+	public List<Map<String, Object>> getAspects(IAspectContainer container) {
+		if (container == null) return null;
 		AspectList aspectList = container.getAspects();
-		if (aspectList == null || aspectList.size() == 0) { return null; }
-		Aspect[] aspectArray = aspectList.getAspects();
-		ArrayList<HashMap<String, Object>> aspectNames = new ArrayList<HashMap<String, Object>>(aspectArray.length);
-		for (Aspect aspect : aspectArray) {
-			HashMap<String, Object> aspectDetails = new HashMap<String, Object>(2);
+		if (aspectList == null) return null;
+		List<Map<String, Object>> aspectNames = Lists.newArrayList();
+		for (Aspect aspect : aspectList.getAspects()) {
+			Map<String, Object> aspectDetails = Maps.newHashMap();
 			aspectDetails.put("name", aspect.getName());
 			aspectDetails.put("quantity", aspectList.getAmount(aspect));
 			aspectNames.add(aspectDetails);
 		}
 		return aspectNames;
+	}
+
+	@LuaMethod(returnType = LuaType.TABLE, description = "Get the map of aspects stored in the block (summed, if there are multiple entries)")
+	public Map<String, Integer> getAspectsSum(IAspectContainer container) {
+		AspectList aspectList = container.getAspects();
+		if (aspectList == null) return null;
+		Map<String, Integer> result = Maps.newHashMap();
+		for (Aspect aspect : aspectList.getAspects()) {
+			String name = aspect.getName();
+			int amount = Objects.firstNonNull(result.get(name), 0);
+			result.put(name, amount + aspectList.getAmount(aspect));
+		}
+		return result;
 	}
 }
