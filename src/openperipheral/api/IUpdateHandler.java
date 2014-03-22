6c18fcd97efe83c1fcf823f15642f9d99852f67f
Bartek Bok <bartek.bok@gmail.com>
2014-03-23 00:45:24 +0100

    Nothing to rescue

--- src/openperipheral/api/IUpdateHandler.java
+++
@@ -1,8 +0,0 @@
-package openperipheral.api;
-
-/**
- * If wrapped object implements this interface, OpenPeripheral will pass HostedPeripheral update calls to it
- */
-public interface IUpdateHandler {
-	public void onPeripheralUpdate();
-}
