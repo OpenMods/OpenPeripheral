d228a702e0ddd17c214ba89063b55213db0c5988
Bartek Bok <bartek.bok@gmail.com>
2014-03-30 14:50:48 +0200

    Unwrap exceptions thrown in proxy

--- src/openperipheral/adapter/peripheral/ProxyAdapterPeripheral.java
+++ src/openperipheral/adapter/peripheral/ProxyAdapterPeripheral.java
@@ -1,9 +1,13 @@
 package openperipheral.adapter.peripheral;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import openperipheral.adapter.composed.ClassMethodsList;
+
+import com.google.common.base.Throwables;
+
 import dan200.computercraft.api.peripheral.IPeripheral;
 
 public class ProxyAdapterPeripheral extends AdapterPeripheral implements InvocationHandler {
@@ -14,8 +18,13 @@ public class ProxyAdapterPeripheral extends AdapterPeripheral implements Invocat
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		if (method.getDeclaringClass() == IPeripheral.class) return method.invoke(this, args);
-		return method.invoke(targetObject, args);
+		try {
+			if (method.getDeclaringClass() == IPeripheral.class) return method.invoke(this, args);
+			return method.invoke(targetObject, args);
+		} catch (InvocationTargetException e) {
+			Throwable wrapper = e.getCause();
+			throw Throwables.propagate(wrapper != null? wrapper : e);
+		}
 	}
 
 }
