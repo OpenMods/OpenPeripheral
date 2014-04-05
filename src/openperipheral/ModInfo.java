7dd837fc808b9ae02bbbff5182d6cbbf8caea4d0
Bartek Bok <bartek.bok@gmail.com>
2014-04-05 14:44:33 +0200

    Use proper CC version id (this only works with versions older than CC1.6, but we can't do nothing about that)

--- src/openperipheral/ModInfo.java
+++ src/openperipheral/ModInfo.java
@@ -7,5 +7,5 @@ public class ModInfo {
 	public static final String VERSION = "@VERSION@";
 	public static final String PROXY_SERVER = "openperipheral.core.CommonProxy";
 	public static final String PROXY_CLIENT = "openperipheral.core.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods@[0.4,];required-after:ComputerCraft@[1.6,];after:AppliedEnergistics;after:IC2;after:EnderStorage;after:BuildCraft|Core;after:Forestry;after:Mystcraft;after:ProjRed|Transmission;after:Railcraft;after:Thaumcraft;after:ThermalExpansion;after:SGCraft;";
+	public static final String DEPENDENCIES = "required-after:OpenMods@[0.4,];required-after:ComputerCraft@[1.60,];after:AppliedEnergistics;after:IC2;after:EnderStorage;after:BuildCraft|Core;after:Forestry;after:Mystcraft;after:ProjRed|Transmission;after:Railcraft;after:Thaumcraft;after:ThermalExpansion;after:SGCraft;";
 }
