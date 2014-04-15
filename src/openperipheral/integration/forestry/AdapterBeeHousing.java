c08fb23eed7d880ee571c1dd3029ce22e09454b2
Bartek Bok <bartek.bok@gmail.com>
2014-04-15 18:12:23 +0200

    This will probably break some bee programs. I probably don't care

--- src/openperipheral/integration/forestry/AdapterBeeHousing.java
+++ src/openperipheral/integration/forestry/AdapterBeeHousing.java
@@ -1,11 +1,13 @@
 package openperipheral.integration.forestry;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.minecraft.item.ItemStack;
 import openperipheral.api.*;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import forestry.api.apiculture.IBeeHousing;
@@ -74,38 +76,70 @@ public class AdapterBeeHousing implements IPeripheralAdapter {
 		return result;
 	}
 
-	@LuaMethod(
-			returnType = LuaType.TABLE,
-			description = "Get the parents for a particular mutation",
-			args = {
-					@Arg(name = "childType", description = "The type of bee you want the parents for", type = LuaType.STRING)
-			})
-	public Map<Integer, Map<String, Object>> getBeeParents(IBeeHousing housing, String childType) {
+	@LuaCallable(returnTypes = LuaType.TABLE, description = "Get all known bees mutations")
+	public List<Map<String, String>> listAllSpecies(IBeeHousing housing) {
 		ISpeciesRoot beeRoot = AlleleManager.alleleRegistry.getSpeciesRoot("rootBees");
-		if (beeRoot == null) { return null; }
-		int i = 1;
-		Map<Integer, Map<String, Object>> result = Maps.newHashMap();
+		if (beeRoot == null) return null;
+		List<Map<String, String>> result = Lists.newArrayList();
+
 		for (IMutation mutation : beeRoot.getMutations(false)) {
 			IAllele[] template = mutation.getTemplate();
-			if (template == null || template.length < 1) {
-				continue;
+			if (template != null && template.length > 0) {
+				IAllele allele = template[0];
+				if (allele instanceof IAlleleSpecies) result.add(serializeSpecies((IAlleleSpecies)allele));
 			}
-			if (template[0].getName().toLowerCase().equals(childType.toLowerCase())) {
-				HashMap<String, Object> parentMap = new HashMap<String, Object>();
-				IAllele allele1 = mutation.getAllele0();
-				if (allele1 != null) {
-					parentMap.put("allele1", allele1.getName());
-				}
-				IAllele allele2 = mutation.getAllele1();
-				if (allele2 != null) {
-					parentMap.put("allele2", allele2.getName());
-				}
-				parentMap.put("chance", mutation.getBaseChance());
-				parentMap.put("specialConditions", mutation.getSpecialConditions());
-				result.put(i++, parentMap);
+		}
+
+		return result;
+	}
+
+	@LuaCallable(returnTypes = LuaType.TABLE, description = "Get the parents for a particular mutation")
+	public List<Map<String, Object>> getBeeParents(IBeeHousing housing,
+			@Arg(name = "childType", description = "The type of bee you want the parents for", type = LuaType.STRING) String childType) {
+		ISpeciesRoot beeRoot = AlleleManager.alleleRegistry.getSpeciesRoot("rootBees");
+		if (beeRoot == null) return null;
+		List<Map<String, Object>> result = Lists.newArrayList();
+		childType = childType.toLowerCase();
+
+		for (IMutation mutation : beeRoot.getMutations(false)) {
+			IAllele[] template = mutation.getTemplate();
+			if (template == null || template.length < 1) continue;
+
+			IAllele allele = template[0];
+
+			if (!(allele instanceof IAlleleSpecies)) continue;
+
+			IAlleleSpecies species = (IAlleleSpecies)allele;
+			final String uid = species.getUID().toLowerCase();
+			final String localizedName = species.getName().toLowerCase();
+
+			if (localizedName.equals(childType) || uid.equals(childType)) {
+				Map<String, Object> parentMap = serializeMutation(mutation);
+				result.add(parentMap);
 			}
 		}
 		return result;
+	}
+
+	private static Map<String, String> serializeSpecies(IAlleleSpecies species) {
+		Map<String, String> result = Maps.newHashMap();
+		result.put("name", species.getName());
+		result.put("uid", species.getUID());
+		return result;
+	}
+
+	private static Map<String, Object> serializeMutation(IMutation mutation) {
+		Map<String, Object> parentMap = Maps.newHashMap();
+
+		IAllele allele1 = mutation.getAllele0();
+		if (allele1 instanceof IAlleleSpecies) parentMap.put("allele1", serializeSpecies((IAlleleSpecies)allele1));
+
+		IAllele allele2 = mutation.getAllele1();
+		if (allele2 instanceof IAlleleSpecies) parentMap.put("allele2", serializeSpecies((IAlleleSpecies)allele2));
+
+		parentMap.put("chance", mutation.getBaseChance());
+		parentMap.put("specialConditions", mutation.getSpecialConditions());
+		return parentMap;
 	}
 
 }
