9d36f8ea873abddc262e7c96e0e8fc5bed911261
Bartek Bok <bartek.bok@gmail.com>
2015-02-04 22:23:55 +0100

    Fix generic type adapter

--- src/main/java/openperipheral/converter/GenericConverterAdapter.java
+++ src/main/java/openperipheral/converter/GenericConverterAdapter.java
@@ -5,13 +5,14 @@ import java.lang.reflect.Type;
 import openperipheral.api.converter.IConverter;
 import openperipheral.api.converter.IGenericTypeConverter;
 
+import com.google.common.reflect.TypeToken;
+
 public abstract class GenericConverterAdapter implements IGenericTypeConverter {
 
 	@Override
 	public Object fromLua(IConverter registry, Object obj, Type expected) {
-		if (expected instanceof Class) return fromLua(registry, obj, (Class<?>)expected);
-
-		return null;
+		TypeToken<?> type = TypeToken.of(expected);
+		return fromLua(registry, obj, type.getRawType());
 	}
 
 	protected abstract Object fromLua(IConverter registry, Object obj, Class<?> expected);
