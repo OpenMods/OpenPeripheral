99951f3a55849328ee8f4fc4d92dad5466fa2676
Bartek Bok <bartek.bok@gmail.com>
2015-03-01 15:04:56 +0100

    Update dependencies (CC 1.7, OC 1.5)

--- src/main/java/openperipheral/ModInfo.java
+++ src/main/java/openperipheral/ModInfo.java
@@ -7,5 +7,5 @@ public class ModInfo {
 	public static final String VERSION = "$VERSION$";
 	public static final String PROXY_SERVER = "openperipheral.core.CommonProxy";
 	public static final String PROXY_CLIENT = "openperipheral.core.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods@[$LIB-VERSION$];after:ComputerCraft@[1.64,];after:OpenComputers@[1.4.5,];";
+	public static final String DEPENDENCIES = "required-after:OpenMods@[$LIB-VERSION$];after:ComputerCraft@[1.70,];after:OpenComputers@[1.5.0,];";
 }
