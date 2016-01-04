9e0d317ebf5f679ed3b13d58d06f425cff38edff
Bartek Bok <bartek.bok@gmail.com>
2016-01-04 15:17:32 +0100

    Switch to BlockPos

--- src/main/java/openperipheral/api/adapter/IWorldPosProvider.java
+++ src/main/java/openperipheral/api/adapter/IWorldPosProvider.java
@@ -1,12 +1,10 @@
 package openperipheral.api.adapter;
 
+import net.minecraft.util.BlockPos;
+
 /**
  * This interface is no longer used by OpenPeripheralCore, but is still used by some child mods (like OpenPeriperal-Integration)
  */
 public interface IWorldPosProvider extends IWorldProvider {
-	public int getX();
-
-	public int getY();
-
-	public int getZ();
+	public BlockPos getPos();	
 }
