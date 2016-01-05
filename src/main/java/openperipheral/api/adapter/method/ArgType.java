ce5236bfb802e91e94a76569b44bab2fab78b95a
Bartek Bok <bartek.bok@gmail.com>
2016-01-05 21:02:53 +0100

    Fix sources path

--- src/main/java/openperipheral/api/adapter/method/ArgType.java
+++ src/main/java/openperipheral/api/adapter/method/ArgType.java
@@ -2,6 +2,8 @@ package openperipheral.api.adapter.method;
 
 import java.util.Locale;
 
+import openperipheral.api.adapter.ITypeClassifier;
+
 public enum ArgType {
 
 	TABLE,
@@ -12,7 +14,7 @@ public enum ArgType {
 	OBJECT,
 	/**
 	 * <p>
-	 * This value usually causes OpenPeripheral to try to automatically deduct type of parameter. Few basic rules exist (for primitive types, arrays, collections), but they can be extended via {@link ITypeQualifier}.
+	 * This value usually causes OpenPeripheral to try to automatically deduct type of parameter. Few basic rules exist (for primitive types, arrays, collections), but they can be extended via {@link ITypeClassifier}.
 	 * </p>
 	 *
 	 * <p>
