787fb75bae9277fe523d3db162fddb800f2bfa86
Bartek Bok <bartek.bok@gmail.com>
2016-01-16 10:26:16 +0100

    CC API update to 1.77

--- src/api/java/dan200/computercraft/api/ComputerCraftAPI.java
+++ src/api/java/dan200/computercraft/api/ComputerCraftAPI.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of the public ComputerCraft API - http://www.computercraft.info
- * Copyright Daniel Ratcliffe, 2011-2015. This API may be redistributed unmodified and in full only.
+ * Copyright Daniel Ratcliffe, 2011-2016. This API may be redistributed unmodified and in full only.
  * For help using the API, and posting your mods, visit the forums at computercraft.info.
  */
 
@@ -48,7 +48,7 @@ public final class ComputerCraftAPI
 
     public static String getAPIVersion()
     {
-        return "1.76";
+        return "1.77";
     }
 
 	/**
