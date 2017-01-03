8b0f14ffbb7058da02a00d94ad657e286efc52bd
Bartek Bok <bartek.bok@gmail.com>
2017-01-03 20:40:19 +0100

    Lowercase modid

--- src/main/java/openperipheral/ModInfo.java
+++ src/main/java/openperipheral/ModInfo.java
@@ -1,12 +1,12 @@
 package openperipheral;
 
 public class ModInfo {
-	public static final String ID = "OpenPeripheralCore";
+	public static final String ID = "openperipheralcore";
 	public static final String API_ID = "OpenPeripheralApi";
 	public static final String NAME = "OpenPeripheralCore";
 	public static final String VERSION = "$VERSION$";
 	public static final String API_VERSION = "$OP-API-VERSION$";
 	public static final String PROXY_SERVER = "openperipheral.core.CommonProxy";
 	public static final String PROXY_CLIENT = "openperipheral.core.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods@[$LIB-VERSION$,$NEXT-LIB-VERSION$);after:ComputerCraft@[1.80,];after:OpenComputers@[1.6.0,];";
+	public static final String DEPENDENCIES = "required-after:openmods@[$LIB-VERSION$,$NEXT-LIB-VERSION$);after:ComputerCraft@[1.80,];after:OpenComputers@[1.6.0,];";
 }
